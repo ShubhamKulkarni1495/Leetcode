f75f1cf74d8afc11a411d4370f5e11aaeebfd761
ShubhamKulkarni1495 <86525504+ShubhamKulkarni1495@users.noreply.github.com>
2022-02-10 22:48:29 +0530

    Heap Tree

---
+++ maxHeap.py
@@ -0,0 +1,53 @@
+def max_heap(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2 
+    
+    if l < n and arr[i] < arr[l]:
+        largest = l
+    
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+    
+    if largest != i:
+        arr[i],arr[largest] = arr[largest],arr[i]
+        max_heap(arr, n, largest)
+# def max-heap has O(1) Time Complexity
+
+def insert(array, newNum):
+    size = len(array)
+    if size == 0:
+        array.append(newNum)
+    else:
+        array.append(newNum);
+        for i in range((size//2)-1, -1, -1):
+            max_heap(array, size, i)
+# def insert has O(n) Time Complexcity
+
+def delete(array, num):
+    size = len(array)
+    i = 0
+    for i in range(0, size):
+        if num == array[i]:
+            break
+        
+    array[i], array[size-1] = array[size-1], array[i]
+
+    array.remove(num)
+    
+    for i in range((len(array)//2)-1, -1, -1):
+        max_heap(array, len(array), i)
+# def delete has O(n) Time Complexcity
+  
+arr = []
+
+insert(arr, 3)
+insert(arr, 4)
+insert(arr, 9)
+insert(arr, 5)
+insert(arr, 2)
+
+print ("Max-Heap array: " + str(arr))
+
+delete(arr,9)
+print("After deleting an element: " + str(arr))
